7ddf0fdae5694aeca0cd7407551680c3c9d4fa97
crduque <crduque@hotmail.com>
2020-10-22 18:03:37 +0000

    Counter working correctly

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,11 +3,16 @@ import { MyNumber } from "./number.jsx";
 
 //create your first component
 export function Home() {
+	let one = 1000;
+	let ten = 10000;
+	let hundred = 100000;
+	let thousand = 1000000;
 	return (
 		<div className="mt-5">
 			<p>
-				Llevas mirando este contador <MyNumber /> <MyNumber />{" "}
-				<MyNumber /> <MyNumber /> segundos.
+				Llevas mirando este contador <MyNumber lotOfCeros={thousand} />{" "}
+				<MyNumber lotOfCeros={hundred} /> <MyNumber lotOfCeros={ten} />{" "}
+				<MyNumber lotOfCeros={one} /> segundos.
 			</p>
 		</div>
 	);
--- src/js/component/number.jsx
+++ src/js/component/number.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { PropTypes } from "prop-types";
 
 export const MyNumber = props => {
 	let [second, setSecond] = useState(0);
 	useEffect(() => {
 		setInterval(() => {
-			setSecond(second++);
-		}, 1000);
+			setSecond(second => second + 1); // no entiendo que es ese arrow
+		}, props.lotOfCeros);
 	}, []);
-	// if (second > 9) {
-	// 	second = setSecond(0);
-	// } else {
-	// 	second = setSecond(second++);
-	// }
-	return <span>{second}</span>;
+
+	return <span>{second % 10}</span>;
+};
+
+MyNumber.propTypes = {
+	lotOfCeros: PropTypes.number
 };
